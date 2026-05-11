7c45cab7d4c62e535bd962db103fa54dc034683f
ottoedsonvillamilangarita-stack <ottoedsonvillamilangarita@gmail.com>
2026-05-11 17:17:53 -0500

    Change Supabase key in utils.py
    Updated Supabase key for configuration.

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import requests
 # CONFIGURACIÓN DE SUPABASE (CAMBIA ESTOS VALORES)
 # ======================================================
 SUPABASE_URL = "https://gpprggvygfempevfzjhi.supabase.co/rest/v1/"  # <-- Pega tu URL aquí
-SUPABASE_KEY = "sb_secret_H7pbtq0_zkPbfu0v4YVepA_A1JhZvz1"  # <-- Pega tu anon key aquí
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpaW96c3RkdnBvaG94dHhnZ2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MjY3NzQsImV4cCI6MjA5MjQwMjc3NH0.0GznsyzaH3D211z7qm_1lNHVNVM0X0KvklCrF4z1BCE"  # <-- Pega tu anon key aquí
 # ======================================================
 
 def get_headers():
