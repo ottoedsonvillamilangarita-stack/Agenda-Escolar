8250ed527d97ccca86a388d091f7a2d543f34f25
ottoedsonvillamilangarita-stack <ottoedsonvillamilangarita@gmail.com>
2026-05-18 10:03:40 -0500

    Fix import path for utils in login.py

--- modulos/login.py
+++ modulos/login.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from modulos.utils import SUPABASE_URL, get_headers
+from utils import SUPABASE_URL, get_headers
 import json  # Añade esta línea
 
 def mostrar():
