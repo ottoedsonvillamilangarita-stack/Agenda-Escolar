001caaacb4257380c9ee8c3de610e2679a1495ca
ottoedsonvillamilangarita-stack <ottoedsonvillamilangarita@gmail.com>
2026-07-27 18:46:10 -0500

    Initialize paneles module with imports

---
+++ modulos/paneles/__init__.py
@@ -0,0 +1,9 @@
+# modulos/paneles/__init__.py
+from . import admin
+from . import docente
+from . import estudiante
+from . import acudiente
+from . import director
+from . import coordinador
+from . import secretaria
+from . import supervisor
