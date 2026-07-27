128f79fd78c2e90679f200a27f512c87b2448433
ottoedsonvillamilangarita-stack <ottoedsonvillamilangarita@gmail.com>
2026-07-27 18:58:45 -0500

    Remove unused imports from __init__.py

--- modulos/__init__.py
+++ modulos/__init__.py
@@ -1,4 +1,3 @@
 # modulos/__init__.py
 from .paneles import admin, docente, estudiante, acudiente, director, coordinador, secretaria, supervisor
-from .compartido import auth, mensajeria
-from .features import horarios
+from .compartido import auth
