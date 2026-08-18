dd1d0168eba110c5a78532e094be984de38190a8
ottoedsonvillamilangarita-stack <ottoedsonvillamilangarita@gmail.com>
2026-08-17 20:56:10 -0500

    Remove unused import statements in __init__.py

--- modulos/paneles/__init__.py
+++ modulos/paneles/__init__.py
@@ -1,8 +1,2 @@
 # modulos/paneles/__init__.py
-from . import docente
-from . import estudiante
-from . import acudiente
-from . import director
-from . import coordinador
-from . import secretaria
-from . import supervisor
+
